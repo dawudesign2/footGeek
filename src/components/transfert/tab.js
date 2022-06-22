982548649f4eb237c0aa5de35645e9c26ca84f83
221179 <estelle.abebanga@sncf.fr>
2022-06-22 15:56:59 +0200

    cleanning project

---
+++ src/components/transfert/tab.js
@@ -0,0 +1,10 @@
+const Tab = () => {
+    return (
+        <>
+           <h2>Mon composantttttttttttttt bordel</h2>
+        </>
+    )
+}
+
+
+export default Tab
--- src/pages/transfert/index.js
+++ src/pages/transfert/index.js
@@ -1,7 +1,9 @@
+import Tab from '../../components/transfert/tab'
 const Transfert = () => {
     return (
         <>
            <h1>Transfert</h1>
+           <Tab/>
         </>
     )
 }
